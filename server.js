42f5f26846940795a8d0b586f8e4651ff3d26df0
Mario_95 <mrojasc95.mr@gmail.com>
2020-02-01 23:48:36 -0500

    Scrapper set up
    scrapper set up done, not yet functional

--- server.js
+++ server.js
@@ -25,3 +25,41 @@ var app = express();
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// using a GET route to carry out our scrapping 
+
+app.get("/scrape", function (req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("http://www.echojs.com/").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+
+        console.log(response)
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("article h2").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href")
+
+        });
+    });
+    // console.log("this is my resut: " + result)
+});
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
