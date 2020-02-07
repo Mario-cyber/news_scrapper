aed069b6b85e3d96c2280be36f29f9ef045c1f04
Mario_95 <mrojasc95.mr@gmail.com>
2020-02-06 21:45:39 -0500

    add mongo_uri variable

--- server.js
+++ server.js
@@ -33,8 +33,11 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+// If deployed, use deployed DB, otherwise use local mongoHEadlines db
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater"
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true
 });
 
